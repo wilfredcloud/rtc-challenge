c37a2ec6f9c755cf0309179c7153ee17d7e32da5
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-19 21:24:10 +0100

    Setup socket io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { io } from "socket.io-client";
 function App() {
+  const SERVER_URL = process.env.REACT_APP_SERVER_BASE_URL as string
+  const createMeeting = () => {
+    const ws = io(SERVER_URL);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={createMeeting}>Create meeting</button>
     </div>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log("It works");
+import express from "express";
+import  http from "http"
+import {Server} from "socket.io"
+import cors from "cors"
+
+const app = express();
+const PORT = 5000;
+app.use(cors)
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors:{
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("user connected");
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
