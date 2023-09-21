8186c4a4b903aceda6663651bd673e2f6ff070b4
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-21 09:33:31 +0100

    feat: Build room UI

--- client/src/context/RoomContext.tsx
+++ client/src/context/RoomContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useEffect, useState } from "react";
+import { ReactNode, createContext, useEffect, useReducer, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Peer from "peerjs";
 import { v4 as uuidV4 } from 'uuid';
@@ -6,14 +6,17 @@ import { v4 as uuidV4 } from 'uuid';
 import socketIOClient, { Socket } from "socket.io-client"
 import { SERVER_BASE_URL, SOCKETEVENTS as SE } from "../utils/constants";
 import { Session } from "../utils/types";
+import { PeerAction, PeerState, peersReducer } from "../reducers/peerReducer";
+import { addPeerAction, removePeerAction } from "../reducers/peerActions";
 
 interface RoomContextValue {
     ws: Socket,
     userPeer: Peer | null,
     stream: MediaStream | null,
+    peers: PeerState
     setUserPeer: React.Dispatch<React.SetStateAction<Peer | null>>;
     setStream: React.Dispatch<React.SetStateAction<MediaStream | null>>;
-
+    dispatchPeers: React.Dispatch<PeerAction>
 }
 const ws: Socket = socketIOClient(SERVER_BASE_URL)
 
@@ -21,8 +24,10 @@ export const RoomContext = createContext<RoomContextValue>({
     ws: ws,
     userPeer: null,
     stream: null,
-    setUserPeer: () => {},
-    setStream: () => {},
+    peers: {},
+    setUserPeer: () => { },
+    setStream: () => { },
+    dispatchPeers: () => {}
 });
 
 
@@ -34,33 +39,35 @@ const RoomProvider: React.FC<RoomProviderProps> = ({ children }) => {
     const navigate = useNavigate();
     const [userPeer, setUserPeer] = useState<Peer | null>(null);
     const [stream, setStream] = useState<MediaStream | null>(null)
+    const [peers, dispatchPeers] = useReducer(peersReducer, {})
     const enterRoomSession = (session: Session) => {
         navigate(`/${session.roomId}/join?session=${session.id}`);
     }
-    const getParticipants = ({ roomId, participants }: { roomId: string, participants: string }) => {
-        console.log(roomId, participants);
+    const getParticipants = ({ roomId, participants }: { roomId: string, participants: string[] }) => {
+       console.log("participannts",participants)
     }
+    const removePeer = ({peerId}: {peerId: string}) => {
+        dispatchPeers(removePeerAction(peerId))
+    }
+
     useEffect(() => {
         const unid = uuidV4();
         const peer = new Peer(unid);
         setUserPeer(peer);
-
+        
         ws.on(SE.roomSessionStarted, enterRoomSession)
         ws.on(SE.roomSessionJoined, getParticipants)
+        ws.on(SE.peerDisconnected, removePeer)
     }, []);
 
-    useEffect (() => {
-        if (!userPeer || !stream) return;
-       ws.on(SE.peerJoined, ({peerId}) => {
-            const call = userPeer.call(peerId, stream);
-       })
+    // useEffect(() => {
+        
+    //     if (!userPeer || !stream) return;
+ 
+    
+    // }, [userPeer, stream])
 
-       userPeer.on("call", (call) => {
-        call.answer(stream)
-       })
-    }, [userPeer, stream])
-
-    return <RoomContext.Provider value={{ ws, userPeer, stream, setUserPeer, setStream }}>
+    return <RoomContext.Provider value={{ ws, userPeer, stream, peers, setUserPeer, setStream, dispatchPeers }}>
         {children}
     </RoomContext.Provider>
 }
