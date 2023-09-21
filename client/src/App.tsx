b37c20b9c548cd04052abbf01e23e964ff1f3ce5
wilfredcloud <wilfredcloudspace@gmail.com>
2023-09-22 00:47:35 +0100

    feat: Add room route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Room from "./pages/Room";
 import RoomSession from "./pages/RoomSession";
+import NewRoom from "./pages/NewRoom";
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/signin" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/newroom" element={<NewRoom/>}/>
         <Route path="/:roomId" element={<Room/>}/>
         <Route path="/:roomId/join" element={<RoomSession/>}/>
       </Routes>
